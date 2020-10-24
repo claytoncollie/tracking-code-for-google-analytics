76a300ad95ce8503990695bdbf2a22d6673174e5
Clayton Collie <clayton.collie@gmail.com>
2020-10-24 08:58:53 -0500

    WP CLI scaffold of new plugin

---
+++ google-analytics-tracking-code.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Google Analytics Tracking Code
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     google-analytics-tracking-code
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Google_Analytics_Tracking_Code
+ */
+
+// Your code starts here.
