d1aa07df12bda7d816981866af84f1eafb0c843b
Clayton Collie <clayton.collie@gmail.com>
2022-06-19 22:08:03 -0500

    add e2d tests

---
+++ tests/cypress/integration/admin.test.js
@@ -0,0 +1,12 @@
+import {deactivatePlugin} from "@10up/cypress-wp-utils/lib/commands/deactivate-plugin";
+
+describe('Admin can login and make sure plugin is activated', () => {
+    before(() => {
+        cy.login();
+    });
+	
+	it('Can activate plugin if it is deactivated', () => {
+        cy.activatePlugin('tracking-code-for-google-analytics');
+        cy.deactivatePlugin('tracking-code-for-google-analytics');
+    });
+});
---
+++ tests/cypress/integration/set-tracking-id-with-option.test.js
@@ -0,0 +1,13 @@
+describe('Set tracking ID with option in admin UI', () => {
+    before(() => {
+        cy.login();
+    });
+
+    it('Can admin set tracking id?', () => {
+        cy.visit('/wp-admin/options-general.php');
+        cy.get('#tracking_code_for_google_analytics').clear().type('option');
+		cy.get('#submit').click();
+
+        cy.get('#tracking_code_for_google_analytics').invoke('val').should('eq', 'option');
+    });
+});
