37fb37b884d3b73819752cfec59b9e195f8f02ab
Clayton Collie <clayton.collie@gmail.com>
2022-06-20 11:39:20 -0500

    Remove test url

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -28,6 +28,6 @@ module.exports = async (on, config) => {
       config.baseUrl = wpEnvConfig.env.tests.config.WP_TESTS_DOMAIN;
     }
   }
-config.baseUrl = 'https://tracking.local';
+
   return config;
 };
