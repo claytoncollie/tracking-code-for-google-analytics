d35fc423a34e0a3118f3ea12bff9119176d366b1
Clayton Collie <clayton.collie@gmail.com>
2023-04-23 16:47:11 -0500

    new config

--- tests/cypress/config.js
+++ tests/cypress/config.js
@@ -30,7 +30,7 @@ const setBaseUrl = async (on, config) => {
 		const port = wpEnvConfig.env.tests.port || null;
 
 		if (port) {
-			config.baseUrl = 'http://tracking.local/';
+			config.baseUrl = 'http://localhost:80/';
 		}
 	}
 
